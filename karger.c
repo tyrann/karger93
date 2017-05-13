73ece42c2bb30b53732fd43ff3e7fd84ab7f961b
tyrann <thomas.j.rubeli@gmail.com>
2017-05-13 19:11:19 +0200

    Basic edge list

---
+++ karger.c
@@ -0,0 +1,23 @@
+
+struct edge {
+	int ep1, ep2;
+	
+};
+
+struct graph{
+	
+	struct edge* edge_list;
+
+	int num_vertices;
+	int num_edges;
+};
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
