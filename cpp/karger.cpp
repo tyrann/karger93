7d724c007e0662eca185649a9b922e2ebfcaf9ee
tyrann <thomas.j.rubeli@gmail.com>
2017-05-15 20:05:49 +0200

    Get the 2 last cluster of vertices

--- cpp/karger.cpp
+++ cpp/karger.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <set>
 #include <unistd.h>
+#include <assert.h>
 
 using namespace std;
 
@@ -23,17 +24,17 @@ struct graph{
 };
 
 
-pair<int,int> run_karger(struct graph* g){
+pair<set<int>,set<int>> run_karger(struct graph* g){
 	int V = g->num_vertices;
 	int E = g->num_edges;
-	std::vector<struct edge> local_edge_list = g->edge_list;
+	vector<struct edge> local_edge_list = g->edge_list;
 
 
 	//Create a collection of sets
-	vector <set<int>> v_set = vector<set<int>>(g->num_vertices) ;
+	vector <set<int>> v_set = vector<set<int>>(g->num_vertices+1) ;
 
 	//Add each vertex in its own set
-	for (int i = 0; i < V; ++i) {
+	for (int i = 0; i <= V; ++i) {
 
 		v_set[i].insert(i);
 		
@@ -44,39 +45,46 @@ pair<int,int> run_karger(struct graph* g){
 		//Pick edge at random
 		struct edge e = local_edge_list[rand() % E];
 
-		cout << e.endpoints.first << " <-> " << e.endpoints.second << endl;
+		//cout << e.endpoints.first << " <-> " << e.endpoints.second << endl;
 		
 		//Check where endpoint are
 		vector<set<int>*>sets;
 		
 		for( auto &s : v_set){
-			if(s.find(e.endpoints.first) != s.end() || s.find(e.endpoints.second) != s.end()){
-				cout << "Found element in set" << endl;
-				cout << *(s.begin()) << endl;
+			if(s.find(e.endpoints.first) != s.end()){
+				//cout << "Found first endpoint in set" << endl;
+				//cout << *(s.begin()) << endl;
 				sets.push_back(&s);
+				
+			}
+			if(s.find(e.endpoints.second) != s.end()){
+				//cout << "Found first endpoint in set" << endl;
+				//cout << *(s.begin()) << endl;
+				sets.push_back(&s);
+				
 			}
 
+
 		}
-		cout << "Size of vector: "  <<sets.size() << endl;
-		cout << *(sets[0]->begin()) << " && " << *(sets[1]->begin()) << endl;
-		cout << "Showing sets:"<< endl;
+		//cout << "Size of vector: "  <<sets.size() << endl;
+		//cout << *(sets[0]->begin()) << " && " << *(sets[1]->begin()) << endl;
+		//cout << "Showing sets:"<< endl;
 
 		for(auto iter=sets[0]->begin(); iter!=sets[0]->end();++iter) {
-			cout << "{"<< (*iter)<<"}" << ", ";
+			//cout << "{"<< (*iter)<<"}" << ", ";
 		}
-		cout << endl;
+		//cout << endl;
 
 		for(auto iter=sets[1]->begin(); iter!=sets[1]->end();++iter) {
-			cout << "{"<< (*iter)<<"}" << ", ";
+			//cout << "{"<< (*iter)<<"}" << ", ";
 		}
 		
-		cout << endl;
+		//cout << endl;
 
-		sleep(1);
 		// Merge subset if different
 		// TODO Do it faster
 		if(!(*sets[0] == *sets[1])){
-			cout << "different sets" << endl;
+			//cout << "different sets" << endl;
 			//Update set
 			sets[0]->insert(sets[1]->begin(),sets[1]->end());
 			//Remove one
@@ -86,10 +94,11 @@ pair<int,int> run_karger(struct graph* g){
 		
 
 	}
-	cout << "Over" << endl;
-	//Retreive size of cut and edges
-	//Rerun until obtaining all min cut
-
+	//cout << "Over" << endl;
+	pair<set<int>,set<int>> last_iter;
+	last_iter.first = v_set[0];
+	last_iter.second = v_set[1];
+	return last_iter;
 }
 
 
@@ -112,11 +121,13 @@ int main(int argc, char *argv[])
 	}
 
 	/*for (auto i = g->edge_list.begin(); i < g->edge_list.end();i++) {
-		cout << (*i).endpoints.first << endl;
+		//cout << (*i).endpoints.first << endl;
 		
 	}*/
+	for(int i = 0; i < 40000; i++){
+	pair <set<int>,set<int>> res = run_karger(g);
+	}
 
-	run_karger(g);
 
 	
 
