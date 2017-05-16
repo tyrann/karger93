3a61d3a3a5180ce131fade2f588ac820326694a6
tyrann <thomas.j.rubeli@gmail.com>
2017-05-16 19:57:44 +0200

    Test for old version

---
+++ cpp/2karger.cpp
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <unistd.h>
+#include <assert.h>
+
+using namespace std;
+
+
+
+struct edge {
+	pair<int,int> endpoints;
+	
+};
+
+struct graph{
+	
+	vector<struct edge> edge_list;
+
+	int num_vertices;
+	int num_edges;
+};
+
+
+int* run_karger(struct graph* g){
+
+	return NULL;
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+	struct graph* g = new graph();
+
+	int vertices, edges;
+	cin >> vertices >> edges;
+
+	g->num_vertices = vertices;
+	g->num_edges = edges;
+	g->edge_list = vector<edge>(edges);
+
+
+	for (int i = 0; i < edges; ++i) {
+		cin >> g->edge_list[i].endpoints.first >> g->edge_list[i].endpoints.second;
+	}
+
+	/*for (auto i = g->edge_list.begin(); i < g->edge_list.end();i++) {
+		//cout << (*i).endpoints.first << endl;
+		
+	}*/
+	for(int i = 0; i < 40000; i++){
+	int* res = run_karger(g);
+	}
+
+
+	
+
+
+	
+	return 0;
+}
