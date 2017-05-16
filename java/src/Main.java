69be0d3ba8ff06a8a071a49e37eb0825be3fa356
tyrann <thomas.j.rubeli@gmail.com>
2017-05-16 22:43:08 +0200

    Test union find

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 
@@ -33,7 +31,7 @@ public class Main {
 		karger();
 		union(1, 2);
 		
-		
+		in.close();	
 		return;
 	}
 	
